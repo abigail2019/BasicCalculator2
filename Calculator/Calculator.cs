313d90b0f981d2dd44fe3f05542b0ed237a63cd2
abigail <abigailnankamba@gmail.com>
2020-01-07 18:38:46 +0200

    Feature: Added calculator with addition and division functionality

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return Addition.Sum(a,b);
+        }
+        public int Divid(int a, int b)
+        {
+            return Division.Divsion(a, b);
+        }
+    }
+}
